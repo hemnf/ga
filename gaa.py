b2c3de4008acb8e72fd9acca141f95ace60fc95f
Hemin Ibrahim <hemn@c4kurd.com>
2020-07-06 21:25:46 +0800

    Update gaa.py

--- gaa.py
+++ gaa.py
@@ -1,21 +1,26 @@
+def funn(v):
+    global c
+    global normalize
+    global myK
+    sig = v[0]
+    if sig <= 0:
+        sig = 0.0001
+    K = int(v[1])
+    if K <= 0:
+        K = 1
+    elif K > myK:
+        K = myK
+    nca = NeighborhoodComponentsAnalysis(random_state=42, sigma=sig)
+    nca.fit(X_train, y_train)
 
-import numpy as np
-import global_align as ga
+    # df1 = pd.DataFrame(nca.transform(X_train))
+    # df1.to_csv("X_train_embed_{}_nca.csv".format(db_name), index=False, header=False)
+    # df2 = pd.DataFrame(nca.transform(X_test))
+    # df2.to_csv("X_test_embed_{}_nca.csv".format(db_name), index=False, header=False)
 
-def test1(d=10, T1=100, T2=120):
-  np.random.seed(1)
-  # generate two random time series of dimension d and duration T1, resp. T2
-  seq1 = np.random.rand(T1, d)
-  seq2 = np.random.rand(T2, d)
-  # define the sigma parameter
-  sigma = 0.5*(T1+T2)/2*np.sqrt((T1+T2)/2)
-  # compute the global alignment kernel value for different triangular parameters
-  diff_t = np.abs(T1-T2)
-  Ts = sorted(set([0, 1, diff_t/2, diff_t, diff_t+1, diff_t*3/2, diff_t*2]))
-  for triangular in Ts:
-    val = ga.tga_dissimilarity(seq1, seq2, sigma, triangular)
-    kval = np.exp(-val)
-    if 0 < triangular <= diff_t:
-      # for 0 < triangular <= diff_t, exp(-tga_d) == 0
-      assert kval == 0
-    print ("T=%d \t exp(-tga_d)=%0.5f" % (triangular, kval))
+    knn = KNeighborsClassifier(n_neighbors=K)
+
+
+    knn.fit(nca.transform(X_train), y_train)
+
+    return 1 - knn.score(nca.transform(X_test), y_test)
